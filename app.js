cfe3585785d9d39437cc090bde8be887c7ef5d3c
Nikhil <kumarnikhilsingh.hac07@gmail.com>
2021-01-03 17:04:24 +0530

    approx ready

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
 key_count=0;
 score=0;
+touch_count=0;
 value=true;
+
+function timming(){
+    let dino=document.querySelector('.dino')
+    dino.classList.add('dino-jump')
+    if (a=="laptop") {
+        setTimeout(()=>{
+            dino.classList.remove('dino-jump')
+        },800)
+    } 
+    else if(a=="tablet"){
+        setTimeout(()=>{
+            dino.classList.remove('dino-jump')
+        },1200) 
+    }
+    else if(a=="mobile"){
+        setTimeout(()=>{
+            dino.classList.remove('dino-jump')
+        },1400) 
+    }
+}
+
+let a="laptop";
 // window.alert(`welcome to dino game press space bar to start the game. use left arrow key to move left and right arrow key to move right`)
 let audio=new Audio('music.mp3')
 audio.loop=true;
+document.ondblclick=function(e){
+    notice=document.querySelector('.notice')
+        notice.innerHTML="Welcome To Dino Game"
+        // key_count+=1;
+        // console.log(key_count);
+        
+}
+document.onclick=function(e){
+    touch_count+=1;
+    if (touch_count==1) {
+        let dragon=document.querySelector('.dragon');
+        dragon.classList.add('animation')
+     }
+    else {
+            let dino=document.querySelector('.dino')
+            dino.classList.add('dino-jump')
+            timming()
+         } 
+         console.log(touch_count);
+}
 document.onkeydown=function(e){
     audio.play()
     console.log(e.keyCode);
@@ -17,11 +60,9 @@ document.onkeydown=function(e){
         dragon.classList.add('animation')
          } 
          else {
-        let dino=document.querySelector('.dino')
-        dino.classList.add('dino-jump')
-        setTimeout(()=>{
-            dino.classList.remove('dino-jump')
-        },800)
+            let dino=document.querySelector('.dino')
+            dino.classList.add('dino-jump')
+            timming()
         }
         
     }
@@ -49,7 +90,7 @@ laptop=setInterval(()=>{
 
     let ox=parseInt(window.getComputedStyle(dragon,null).getPropertyValue('left'))
     let oy=parseInt(window.getComputedStyle(dragon,null).getPropertyValue('top'))
-    console.log(dx,dy,ox,oy);
+    // console.log(dx,dy,ox,oy);
     offsetx=Math.abs(dx-ox);
     offsety=Math.abs(dy-oy);
     // console.log(offsetx);
@@ -57,6 +98,7 @@ laptop=setInterval(()=>{
     if (offsetx<120 && offsety<52) {
         dragon.classList.remove('animation')
         key_count=0;
+        touch_count=0;
       
         
         notice.innerHTML=`<span >gameover-Double-tap-reload</span><br> <p class="score-design" > your score ${score}</p> <br> <p class="name" > created by sridhar</p>`
@@ -98,3 +140,54 @@ leftBtn.addEventListener('click',function(e){
         dino.style.left=(dinox-112)+"px"
     }
 })
+
+const media=window.matchMedia("(max-width:667px)")
+if(media.matches){
+    clearInterval(laptop)
+    tablet=setInterval(()=>{
+        dino=document.querySelector('.dino');
+        
+        dragon=document.querySelector('.dragon')
+        let dx=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'))
+        let dy=parseInt(window.getComputedStyle(dino,null).getPropertyValue('top'))
+    
+        let ox=parseInt(window.getComputedStyle(dragon,null).getPropertyValue('left'))
+        let oy=parseInt(window.getComputedStyle(dragon,null).getPropertyValue('top'))
+        console.log(dx,dy,ox,oy);
+        offsetx=Math.abs(dx-ox);
+        offsety=Math.abs(dy-oy);
+        console.log(offsetx);
+        console.log(offsety);
+        if (offsetx<110 && offsety<40) {
+            dragon.classList.remove('animation')
+            key_count=0;
+          
+            
+            notice.innerHTML=`<span >gameover-Double-tap-reload</span><br> <p class="score-design" > your score ${score}</p> <br> <p class="name" > created by sridhar</p>`
+            score=0;
+            score.innerHTML=0;
+            audio.pause()
+            let audio2=new Audio('gameover.mp3')
+            audio2.play()
+            setTimeout(()=>{
+                audio2.pause()
+                audio.play()
+            },1500)
+        }
+        else if(offsetx<140 &&value){
+            score+=1
+            input_score=document.querySelector('.result');
+            input_score.innerHTML=score;
+            value=false;
+            setTimeout(()=>{
+                value=true;
+            },1000)
+        }
+    },100)
+    a="tablet";
+}
+const media1=window.matchMedia("(max-width:420px)")
+if (media1.matches) {
+    console.log(media1);
+    a="mobile";
+}
