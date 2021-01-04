9c5bfc0d6ba8761f87a174d09a0a491bd2b393c6
Nikhil <kumarnikhilsingh.hac07@gmail.com>
2021-01-04 09:58:15 +0530

    ready

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ key_count=0;
 score=0;
 touch_count=0;
 value=true;
+position_change="laptop"
 
 function timming(){
     let dino=document.querySelector('.dino')
@@ -23,6 +24,31 @@ function timming(){
     }
 }
 
+function positiontoleft(){
+    let dinox=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+    let scoreboard=document.querySelector('.scoreboard')
+    let scoreboardx=parseInt(window.getComputedStyle(scoreboard,null).getPropertyValue('left'));
+    console.log(scoreboardx)
+    mathposition=110;
+    positionfixed=scoreboardx-mathposition
+    dino=document.querySelector('.dino')
+    if (dinox<positionfixed&&position_change=="laptop") {
+        dino.style.left=(dinox+112)+"px"
+    }
+    else if(dinox<positionfixed&&position_change=="tablet"){
+        dino.style.left=(dinox+58)+"px"
+    }
+    else if(dinox<positionfixed&&position_change=="mobile"){
+        dino.style.left=(dinox+30)+"px"
+    }
+}
+function positiontoright(){
+    let dinox=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+        dino=document.querySelector('.dino')
+        if (dinox>0) {
+            dino.style.left=(dinox-112)+"px"
+        }
+}
 let a="laptop";
 // window.alert(`welcome to dino game press space bar to start the game. use left arrow key to move left and right arrow key to move right`)
 let audio=new Audio('music.mp3')
@@ -67,18 +93,10 @@ document.onkeydown=function(e){
         
     }
     else if(e.keyCode==39){
-        let dinox=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
-        dino=document.querySelector('.dino')
-        if (dinox<1232) {
-            dino.style.left=(dinox+112)+"px"
-        }
+      positiontoleft()
     }
     else if(e.keyCode==37){
-        let dinox=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
-        dino=document.querySelector('.dino')
-        if (dinox>0) {
-            dino.style.left=(dinox-112)+"px"
-        }
+     positiontoright()   
     }
 }
 laptop=setInterval(()=>{
@@ -99,8 +117,7 @@ laptop=setInterval(()=>{
         dragon.classList.remove('animation')
         key_count=0;
         touch_count=0;
-      
-        
+        notice=document.querySelector('.notice')
         notice.innerHTML=`<span >gameover-Double-tap-reload</span><br> <p class="score-design" > your score ${score}</p> <br> <p class="name" > created by sridhar</p>`
         score=0;
         score.innerHTML=0;
@@ -126,19 +143,12 @@ laptop=setInterval(()=>{
 let rightBtn=document.getElementById("mybutton2");
 // console.log(right);
 rightBtn.addEventListener('click', function (e) {
-    let dinox=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
-    dino=document.querySelector('.dino')
-    if (dinox<1232) {
-        dino.style.left=(dinox+112)+"px"
-    }
+    positiontoleft()
   });
 let leftBtn=document.getElementById('mybutton1');
 leftBtn.addEventListener('click',function(e){
-    let dinox=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
-    dino=document.querySelector('.dino')
-    if (dinox>0) {
-        dino.style.left=(dinox-112)+"px"
-    }
+   
+    positiontoright()
 })
 
 const media=window.matchMedia("(max-width:667px)")
@@ -161,6 +171,7 @@ if(media.matches){
         if (offsetx<110 && offsety<40) {
             dragon.classList.remove('animation')
             key_count=0;
+            touch_count=0;
           
             
             notice.innerHTML=`<span >gameover-Double-tap-reload</span><br> <p class="score-design" > your score ${score}</p> <br> <p class="name" > created by sridhar</p>`
@@ -185,9 +196,54 @@ if(media.matches){
         }
     },100)
     a="tablet";
+    position_change="tablet";
 }
 const media1=window.matchMedia("(max-width:420px)")
 if (media1.matches) {
     console.log(media1);
     a="mobile";
-}
+    position_change="mobile"
+    clearInterval(tablet)
+    mobile1=setInterval(()=>{
+        dino=document.querySelector('.dino');
+        
+        dragon=document.querySelector('.dragon')
+        let dx=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'))
+        let dy=parseInt(window.getComputedStyle(dino,null).getPropertyValue('top'))
+    
+        let ox=parseInt(window.getComputedStyle(dragon,null).getPropertyValue('left'))
+        let oy=parseInt(window.getComputedStyle(dragon,null).getPropertyValue('top'))
+        console.log(dx,dy,ox,oy);
+        offsetx=Math.abs(dx-ox);
+        offsety=Math.abs(dy-oy);
+        console.log(offsetx);
+        console.log(offsety);
+        if (offsetx<95 && offsety<40) {
+            dragon.classList.remove('animation')
+            key_count=0;
+            touch_count=0;
+          
+            
+            notice.innerHTML=`<span >gameover-Double-tap-reload</span><br> <p class="score-design" > your score ${score}</p> <br> <p class="name" > created by sridhar</p>`
+            score=0;
+            score.innerHTML=0;
+            audio.pause()
+            let audio2=new Audio('gameover.mp3')
+            audio2.play()
+            setTimeout(()=>{
+                audio2.pause()
+                audio.play()
+            },1500)
+        }
+        else if(offsetx<140 &&value){
+            score+=1
+            input_score=document.querySelector('.result');
+            input_score.innerHTML=score;
+            value=false;
+            setTimeout(()=>{
+                value=true;
+            },1000)
+        }
+    },100)
+    }
+
